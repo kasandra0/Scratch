266eeac5d5f47eabcaf5bd8c939e8f26204f5b3b
Kasandra Wolf <wolf.kasandra@gmail.com>
2019-02-15 14:24:09 -0700

    comments

--- bucket.js
+++ bucket.js
@@ -1,16 +1,17 @@
-// How many different ways can I add 1's and 2's to to get n
+// How many different ways can I add 1's and 2's to to equal n
 function fillBucket(n) {
   let result = 1; // all ones
   let twos = Math.floor(n / 2);
   let ones = n % 2;
-  if (twos >= 1) {
+  if (twos >= 1) { // if n >= 2
     if (ones == 1) { // n is odd
       let permutations = factorial(ones + twos) / (factorial(ones) * factorial(twos))
       result += permutations;
     }
-    else {
-      result++ // all twos
+    else { //n is even
+      result++ // the only permuation is all twos
     }
+    // split each two into 2 ones
     while (twos > 1) {
       ones = ones + 2;
       twos--;
